f06e6e9c42b66237bf1c6ab390759115aa1a0783
Your Name <charmainemsibi02@gmail.com>
2025-03-03 22:01:27 +0200

    Added basic functionality on registration form

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
 import Dashboard from './components/Dashboard';
+import RegisterPage from './pages/RegisterPage';
 import './index.css';
 
 function App() {
   return (
-    <>
-    <Dashboard/>
-    </>
+    <div className="App">
+      <h1>Welcome to the Registration Page</h1>
+      <RegisterPage/>
+    </div>
   );
 }
 
